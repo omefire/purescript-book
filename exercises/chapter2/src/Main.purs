7f511e16480c640f67bf32a0de8cb43d2b99a443
Omar Mefire <omefire@gmail.com>
2018-06-30 22:18:50 -0500

    Implemented function  and played around with the module

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,10 +1,13 @@
 module Main where
 
+import Effect.Console (logShow)
+import Math (sqrt, pi)
 import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = r * r * pi
 
 main = logShow (diagonal 3.0 4.0)
