6f10c43a2b0085455ccd1fe83bc6cf46e7abc7c6
Omar Mefire <omefire@gmail.com>
2018-06-30 23:10:33 -0500

    Solving exercises for chapter3

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, null, nubBy)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -37,3 +37,24 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+-- head :: List Entry -> Maybe Entry
+-- filterEntry :: Entry -> Boolean
+-- filter :: (Entry -> Boolean) -> List Entry -> List Entry
+
+findEntryByStreetAddress :: String -> AddressBook -> Maybe Entry
+findEntryByStreetAddress street = head <<< filter filterEntryByAddress
+  where
+    filterEntryByAddress :: Entry -> Boolean
+    filterEntryByAddress entry = entry.address.street == street
+
+doesNameAppearInAddressBook :: String -> AddressBook -> Boolean
+doesNameAppearInAddressBook name = null <<< filter filterName
+  where
+    filterName :: Entry -> Boolean
+    filterName entry = (entry.firstName == name || entry.lastName == name)
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates addressBook = nubBy (\e1 e2 -> e1.firstName == e2.firstName && e1.lastName == e2.lastName) addressBook
+
+-- > removeDuplicates (fromFoldable [{firstName: "Omar", lastName: "Mefire", address: {street:"LA", city:"LA", state:"CA"}}, {firstName: "Omar", lastName: "Mefire", address: {street:"LA", city:"LA", state:"CA"}}])
