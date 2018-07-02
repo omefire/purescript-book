a6ec0777b9d6ff23857641abbf6f1de4f048e72e
Omar Mefire <omefire@gmail.com>
2018-07-02 03:32:03 -0500

    Chapter 4, 4.7.1-3

--- exercises/chapter4/src/Exercises.purs
+++ exercises/chapter4/src/Exercises.purs
@@ -2,8 +2,9 @@ module Exercises where
 
 import Prelude
 
-import Data.Array (uncons)
+import Data.Array (uncons, filter)
 import Data.Maybe
+import Data.Ring (negate)
 
 isEven :: Int -> Boolean
 isEven n | n == 1 = false
@@ -21,3 +22,11 @@ countEven xs = countEvenHelper xs 0
                                                                     true -> countEvenHelper xss (count + 1)
                                                                     false -> countEvenHelper xss count
                                     Nothing -> count
+
+squares :: Array Int -> Array Int
+squares xs = map (\x -> x * x) xs
+
+removeNegativeNumbers :: Array Int -> Array Int
+removeNegativeNumbers xs = filter (\x -> x > 0) xs
+
+infixr 5 filter as <$?>
