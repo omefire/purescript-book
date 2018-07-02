aa1747fba8c8df613330d97c2ebb725bf4cf9650
Omar Mefire <omefire@gmail.com>
2018-07-02 03:23:12 -0500

    Chapter 4, Exercise 4.4.2

--- exercises/chapter4/src/Exercises.purs
+++ exercises/chapter4/src/Exercises.purs
@@ -2,6 +2,9 @@ module Exercises where
 
 import Prelude
 
+import Data.Array (uncons)
+import Data.Maybe
+
 isEven :: Int -> Boolean
 isEven n | n == 1 = false
        | n == -1 = false
@@ -9,3 +12,12 @@ isEven n | n == 2 = true
        | n == -2 = true
 isEven n | n < 0 = isEven $ (n + 2)
          | otherwise = isEven $ (n - 2)
+
+countEven :: Array Int -> Int
+countEven xs = countEvenHelper xs 0
+ where
+  countEvenHelper xs count = case uncons xs of 
+                                    Just { head: x, tail: xss } -> case (isEven x) of
+                                                                    true -> countEvenHelper xss (count + 1)
+                                                                    false -> countEvenHelper xss count
+                                    Nothing -> count
